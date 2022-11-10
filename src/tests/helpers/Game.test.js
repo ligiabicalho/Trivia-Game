e2e4618facb38c2c78fe60b47c14dbff2a945684
LeandroDoudement <leandrodoudement@gmail.com>
2022-11-10 00:10:13 -0300

    Correção de erros do Eslint

--- src/tests/helpers/Game.test.js
+++ src/tests/helpers/Game.test.js
@@ -1,12 +1,11 @@
 import renderWithRouterAndRedux from './renderWithRouterAndRedux';
-import Settings from '../../pages/Settings'
 import { screen } from '@testing-library/react';
-import Game from '../../pages/Game'
+import Game from '../../pages/Game';
 
 describe('Testes do componente Game', () => {
-    it('Deve possuir um titulo Game!', () => {
-        renderWithRouterAndRedux(<Game />)
-        const gameTitle = screen.getByText('Game!')
-        expect(gameTitle).toBeInTheDocument()
-    })
-})
+  it('Deve possuir um titulo Game!', () => {
+    renderWithRouterAndRedux(<Game />);
+    const gameTitle = screen.getByText('Game!');
+    expect(gameTitle).toBeInTheDocument();
+  });
+});
--- src/tests/helpers/Settings.test.js
+++ src/tests/helpers/Settings.test.js
@@ -1,12 +1,12 @@
 import renderWithRouterAndRedux from './renderWithRouterAndRedux';
-import Settings from '../../pages/Settings'
+import Settings from '../../pages/Settings';
 import { screen } from '@testing-library/react';
 
 describe('Testes do componente Settings', () => {
-    it('A tela de configurações deve possuir um título', () => {
-        renderWithRouterAndRedux(<Settings />)
-        const settingTitle = screen.getByTestId("settings-title")
-        expect(settingTitle).toBeInTheDocument()
-        expect(settingTitle.innerHTML).toBe('Settings')
-    })
-})
+  it('A tela de configurações deve possuir um título', () => {
+    renderWithRouterAndRedux(<Settings />);
+    const settingTitle = screen.getByTestId('settings-title');
+    expect(settingTitle).toBeInTheDocument();
+    expect(settingTitle.innerHTML).toBe('Settings');
+  });
+});
