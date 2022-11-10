2dd5d3e5634dde3542e6782103c5f4484ee1ee48
LeandroDoudement <leandrodoudement@gmail.com>
2022-11-10 00:39:37 -0300

    Requisito 4 - Testes do componente Header

--- src/tests/helpers/Game.test.js
+++ src/tests/helpers/Game.test.js
@@ -1,11 +1,16 @@
 import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 import { screen } from '@testing-library/react';
-import Game from '../../pages/Game';
+import Game from '../../pages/Game'
 
 describe('Testes do componente Game', () => {
-  it('Deve possuir um titulo Game!', () => {
-    renderWithRouterAndRedux(<Game />);
-    const gameTitle = screen.getByText('Game!');
-    expect(gameTitle).toBeInTheDocument();
+  it('Deve possuir um um componente Header com uma imagem, nome do usuario e um placar', async () => {
+    renderWithRouterAndRedux(<Game />)
+    const headerImage = await screen.findByTestId('header-profile-picture');
+    const headerUsername = await screen.findByTestId('header-player-name');
+    const headerScore = await screen.findByTestId('header-score');
+    expect(headerImage).toBeInTheDocument();
+    expect(headerUsername).toBeInTheDocument();
+    expect(headerScore).toBeInTheDocument();
+    expect(headerScore.innerHTML).toBe('Score: 0');
   });
 });
