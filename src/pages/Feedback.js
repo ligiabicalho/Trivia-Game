4600687a1fc9dbba4e9fd1804f6a280dd367170a
AIMÊ <aimemartins@hotmail.com>
2022-11-11 19:19:40 -0300

    Requisito 14 - finalizada

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { score, assertions } = this.props;
     return (
       <>
         <Header />
         <p data-testid="feedback-text">
           Feedback
         </p>
+        <p data-testid="feedback-total-score">
+          {score}
+        </p>
+        <p data-testid="feedback-total-question">
+          {assertions}
+        </p>
       </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+  score: player.score,
+});
+
+Feedback.propTypes = {
+  score: PropTypes.number,
+  assertions: PropTypes.number,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
