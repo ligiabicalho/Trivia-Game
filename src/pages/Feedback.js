dc02cc69227fde32ad936d969f60fbf6f70b5c4b
Lígia Bicalho <ligiabicalho@gmail.com>
2022-11-11 19:03:43 -0300

    Requisito 15, play again, concluido

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  redirectLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <>
@@ -9,9 +15,22 @@ class Feedback extends React.Component {
         <p data-testid="feedback-text">
           Feedback
         </p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectLogin }
+        >
+          Play Again
+        </button>
       </>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Feedback;
