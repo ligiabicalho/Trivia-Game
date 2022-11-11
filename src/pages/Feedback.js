78fac10434fb3d9fa1ea70a63700794da9cc72cf
LeandroDoudement <leandrodoudement@gmail.com>
2022-11-11 19:11:01 -0300

    Requisito 13 | Feedback Text

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const couldBeBetterLimit = 3;
+    const feedbackText = assertions < couldBeBetterLimit
+      ? 'Could be better...'
+      : 'Well Done!';
     return (
       <>
         <Header />
         <p data-testid="feedback-text">
-          Feedback
+          {feedbackText}
         </p>
       </>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number,
+  assertions: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = ({ player }) => ({
+  score: player.score,
+  assertions: player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
